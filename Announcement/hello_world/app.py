43d7dddd56f6c6e106a903828cb35ca5d87d832f
EC2 Default User <ec2-user@ip-172-31-19-116.us-west-1.compute.internal>
2021-01-29 19:18:53 +0000

    dynamobd updated with details

--- Announcement/hello_world/app.py
+++ Announcement/hello_world/app.py
@@ -5,15 +5,17 @@ from flask import request
 
 
 app1 = FlaskLambda(__name__)
+ddb = boto3.resource('dynamodb')
+table = ddb.Table('AnnouncementsTable')
 
 @app1.route('/test', methods=['GET'])
 def index():
     return json_response({"message": "Hello, world!"})
 
 @app1.route('/announcements', methods=['GET', 'POST'])
-def put_list_students():
+def put_list_announcements():
     if request.method == 'GET':
-        return json_response({"message": "Hello, world!2222"})
+        return json_response(table.scan()['Items'])
     else:
         return json_response({"message": "student entry created"})
 
