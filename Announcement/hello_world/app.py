0ac082b81227024c72cffa143411422abf235a9a
sudhakar <sudhavuppalapati@gmail.com>
2021-05-16 11:20:29 +0000

    updated ExclusiveStartKey

--- Announcement/hello_world/app.py
+++ Announcement/hello_world/app.py
@@ -1,11 +1,12 @@
 import json
 import boto3
 from flask_lambda import FlaskLambda
-from flask import request
-
+from flask import request, jsonify
+from boto3.dynamodb.conditions import Key
 
 app1 = FlaskLambda(__name__)
 dynamodb = boto3.resource("dynamodb", region_name="us-west-1")
+dynamodb1 = boto3.client("dynamodb")
 client = boto3.client('ssm')
 TableNameParameter  =  client.get_parameter(Name='TableName',  WithDecryption= False)
 TableNamevalue = TableNameParameter.get("Parameter").get("Value")
@@ -22,10 +23,36 @@ clientSNS = boto3.client('sns')
 @app1.route('/announcements', methods=['GET'])
 def listAnnouncements():
     if request.method == 'GET':
-        response = table.scan()
-        data = response['Items']
-        # print(data)
-        return json_response(data)
+        error = 0
+       
+        try :
+           key=request.headers["Exclusivestartkey"]    
+           print(key)
+           stratkey = json.loads(key)
+        except:
+            error =1 
+        if error != 1 :
+            response = dynamodb1.scan(TableName = TableNamevalue,Limit=3,ExclusiveStartKey= stratkey)
+        else : 
+            response = dynamodb1.scan(TableName = TableNamevalue,Limit=3)
+        #response = table.query( KeyConditionExpression=Key('title').eq('testannouncement2'), )
+        Count = response['Count']
+        if Count != 0 :
+            data = response['Items']
+            print(response)
+            print("LastEvaluatedKey details : ")
+            try :
+              LastEvaluatedKey = response["LastEvaluatedKey"]
+            except: 
+              error =2  
+            #print(json.loads(LastEvaluatedKey))
+            counts = dict()
+            counts['Items'] = data
+            if error != 2 :
+                counts['LastEvaluatedKey'] = response["LastEvaluatedKey"]
+            return json_response(counts,200)
+        else :
+            return json_response("No data available",200)
 
 @app1.route('/announcements', methods=['POST'])
 def putAnnouncements():
@@ -39,6 +66,7 @@ def putAnnouncements():
         return json_response({"message": "announcements description should not be empty"},400)    
     print(json_object["title"])
     dbResponse = table.put_item( Item= {"title":  title,"description": description,"date":json_object["date"]})
+    #dbResponse = dynamodb1.put_item(TableName = TableNamevalue,Item= {"title":  title,"description": description,"date":json_object["date"]})
     dbreturn = dbResponse.get("ResponseMetadata").get("HTTPStatusCode") 
     if dbreturn != 200 :
         return json_response({"message": "please can you check with your data something went worng with DynamoDB insert data"},dbreturn); 
@@ -50,7 +78,7 @@ def putAnnouncements():
             return json_response({"message": "please can you check with your data something went worng sending notification to user"},responsecode); 
         else :
             print("success")
-    return json_response({"message": "announcements entry created"})
+    return json_response({"message": "announcements entry created"},201)
 
-def json_response(data, response_code=200):
+def json_response(data, response_code):
     return json.dumps(data), response_code, {'Content-Type': 'application/json'}
